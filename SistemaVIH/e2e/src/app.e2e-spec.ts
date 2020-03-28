fd375a10c005731a187abb080b36ba6171443ac0
Alex Martinez <alex_martinez_3355@hotmail.com>
2020-03-28 03:34:29 -0600

    Configuración de bootstrap en el proyecto.

--- SistemaVIH/e2e/src/app.e2e-spec.ts
+++ SistemaVIH/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('SISIOS app is running!');
+    expect(page.getTitleText()).toEqual('SistemaVIH app is running!');
   });
 
   afterEach(async () => {
--- SistemaVIH/src/app/app.component.spec.ts
+++ SistemaVIH/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SISIOS'`, () => {
+  it(`should have as title 'SistemaVIH'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SISIOS');
+    expect(app.title).toEqual('SistemaVIH');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('SISIOS app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('SistemaVIH app is running!');
   });
 });
